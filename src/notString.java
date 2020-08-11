8050677026e596529669e903fe69f888ca1d9786
Jan Gevaert <jan.gevaert@gmail.com>
2020-08-11 06:24:47 -0600

    Initial commit

---
+++ src/notString.java
@@ -0,0 +1,9 @@
+public class notString {
+  public String notString(String str) {
+    if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
+      return str;
+    } else {
+      return "not " + str;
+    }
+  }
+}
